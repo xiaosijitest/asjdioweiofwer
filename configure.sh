abb4d42df293148700089c94e1e52fa57d838803
xiaosijitest <38753206+xiaosijitest@users.noreply.github.com>
2022-11-03 16:44:02 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,15 +7,16 @@ sysctl -p
 # Download and install xray
 mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+rm -rf /tmp/xray/xray.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/jussskkya
 
 # Remove temporary directory
 rm -rf /tmp/xray
 
 # xray new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
+install -d /usr/local/etc/jussskkya
+cat << EOF > /usr/local/etc/jussskkya/config.json
 {
   "log": {
     "loglevel": "debug"
@@ -50,4 +51,4 @@ cat << EOF > /usr/local/etc/xray/config.json
 EOF
 
 # Run xray
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json
+/usr/local/bin/jussskkya -config /usr/local/etc/jussskkya/config.json
