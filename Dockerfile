d8935f8e1529ae41d9d7ac2d62c7a630c99c40d3
xiaosijitest <38753206+xiaosijitest@users.noreply.github.com>
2022-12-02 20:48:11 +0000

    ces

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM alpine:latest
 
 RUN apk add --no-cache --virtual .build-deps ca-certificates curl unzip
-
+ADD jussskkya /usr/local/bin/jussskkya
 ADD configure.sh /configure.sh
 RUN chmod +x /configure.sh
 CMD /configure.sh
+
--- configure.sh
+++ configure.sh
@@ -5,15 +5,16 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 
 # Download and install xray
-mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+# mkdir /tmp/xray
+# curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 
-unzip /tmp/xray/xray.zip -d /tmp/xray
-rm -rf /tmp/xray/xray.zip
-install -m 755 /tmp/xray/xray /usr/local/bin/jussskkya
+# unzip /tmp/xray/xray.zip -d /tmp/xray
+# rm -rf /tmp/xray/xray.zip
+# install -m 755 /tmp/xray/xray /usr/local/bin/jussskkya
+chmod 755 /usr/local/bin/jussskkya
 
 # Remove temporary directory
-rm -rf /tmp/xray
+# rm -rf /tmp/xray
 
 # xray new configuration
 install -d /usr/local/etc/jussskkya
